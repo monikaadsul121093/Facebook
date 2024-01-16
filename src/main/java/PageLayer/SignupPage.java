34c3082b34074653ff24a46a72bb78a6dd0c3660
Rohini23arohi <156347990+Rohini23arohi@users.noreply.github.com>
2024-01-16 15:27:04 +0530

    SignUpPage test case

---
+++ src/main/java/PageLayer/SignupPage.java
@@ -0,0 +1,11 @@
+package PageLayer;
+
+import BaseLayer.BaseClass;
+
+public class SignupPage  extends BaseClass
+{
+	public void signUp()
+	{
+		System.out.println("signUppage testcase");
+	}
+}
---
+++ src/test/java/TestLayer/SignUpPageTest.java
@@ -0,0 +1,16 @@
+package TestLayer;
+
+import BaseLayer.BaseClass;
+import PageLayer.SignupPage;
+
+public class SignUpPageTest extends BaseClass
+{
+
+	public static void validateSignUpFun()
+	{
+		SignupPage signpage =new SignupPage();
+		signpage.signUp();
+					
+				
+	}
+}
