093c18b185dbe02a57d422b8b54ef23bdbc28b45
Tajuddin08 <156346480+Tajuddin08@users.noreply.github.com>
2024-01-16 16:00:16 +0530

    Create Account Test PageCase

---
+++ src/main/java/PageLayer/createAccountPage.java
@@ -0,0 +1,11 @@
+package PageLayer;
+
+import BaseLayer.BaseClass;
+
+
+public class createAccountPage extends BaseClass{
+
+	public void createAccount () {
+		System.out.println("Create Account Page TestCase");
+	}
+}
